a2e6e101673015909967d71ca3781d1ced3e8023
szymonwietowski <szymon1402@wp.pl>
2020-05-10 14:28:40 +0200

    toString bug

--- js/term.js
+++ js/term.js
@@ -13,7 +13,7 @@ class Term {
     toString() {
         if(this.coefficient == 0) return '';
         return `${this.coefficient < 0 ? '-' : '+'} ` +
-               `${Math.abs(this.coefficient) != 1 ? Math.abs(this.coefficient) : ''}` + 
+               `${(Math.abs(this.coefficient) == 1 && this.exponent != 0) ? '' : Math.abs(this.coefficient)}` + 
                `${this.exponent == 0 ? '' : 'x'}` +
                `${(this.exponent == 0 || this.exponent == 1) ? '' : this.exponent.toString().sup()} `;
     }
